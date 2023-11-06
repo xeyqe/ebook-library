a76d93e32599ec4d6b89eae3adbc85b7d9ee2615
xeyqe <xeyqe@seznam.cz>
2023-11-06 22:10:40 +0100

    remove not used files

--- downloader-dep.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if ! [[ -d "dist/downloader" ]]; then
-    git clone -b downloader https://github.com/xeyqe/awesome-cordova-plugins.git dist/downloader
-fi
-
-rm -rf node_modules/@awesome-cordova-plugins/downloader/
-cp -r dist/downloader/downloader/ node_modules/@awesome-cordova-plugins/
